ddd65dc3e6d02f0a8e861912a34dda4e30a1308f
Rene <rene.exe@gmail.com>
2020-08-20 23:17:15 -0300

    Desacoplando configs de ambientes distintos
    Close #11

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '60aaf!8(r591@=ujmzhh*c4z3@^et5q*ejc1_7-sbo%l)hccq_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
