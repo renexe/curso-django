5ddffd74c0b2138fea367935e61d7ddeed317643
Rene <rene.exe@gmail.com>
2020-09-02 23:00:38 -0300

    Transformando métodos test_case do Django em funções
    #Close77

--- pypro/tests/test_home.py
+++ pypro/tests/test_home.py
@@ -1,6 +1,14 @@
 from django.test import Client
 
+from pypro.django_assertions import assert_contains
+
 
 def test_status_code(client: Client):
-    resp = client.get('/home')
+    resp = client.get('/')
     resp.status_code == 200
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Curso Django Pro</title>')
+
