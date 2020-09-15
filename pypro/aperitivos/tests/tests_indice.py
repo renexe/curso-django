dcf59799b89eba0b9a5f5fe705e4fbcd244a45c4
Rene <rene.exe@gmail.com>
2020-09-15 18:23:50 -0300

    Criando página indice com listagem dos vídeos
    #Close 124

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Vídeo Aperitivo: Motivação',
+        'Instalação Windows',
+    ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp, titulo)
