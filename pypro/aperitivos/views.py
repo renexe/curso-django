514ffc207e2c0d2169ff3922e69f07b3ccc11ac5
Rene <rene.exe@gmail.com>
2020-09-15 14:44:31 -0300

    Criando lógica para template video apresentar conteúdo de acordo com slug
    #Close 120

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 74410733}
-
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 74410733},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 74410744},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
