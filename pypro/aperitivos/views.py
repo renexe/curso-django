72714b4c2858e4155d7787ca6397907dc9343028
Rene <rene.exe@gmail.com>
2020-09-15 13:55:58 -0300

    Consertando erros do flake8

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo':'Vídeo Aperitivo: Motivação', 'vimeo_id':74410733}
+    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 74410733}
 
     return render(request, 'aperitivos/video.html', context={'video': video})
